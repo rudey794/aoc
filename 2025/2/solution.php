af3b8d283e93f035e1fc6712b1cd5822d7a0d79c
rudey794 <reubegan@gmail.com>
2025-12-25 15:28:01 +0000

    solve day 2

--- 2025/2/solution.php
+++ 2025/2/solution.php
@@ -1,15 +1,20 @@
 <?php
    function solve($file){
       $part1 = 0;
+      $part2 = 0;
       $lines = explode(",", file_get_contents($file));
       foreach($lines as $line){
          list($start, $end) = array_map('intval', explode("-", $line));
          for($i = (int)$start; $i <= (int)$end; $i++){
-            if(preg_match("/^(\d+)\1$/", (string)$i)){
+            if(preg_match('/^(\d+)\1$/', (string)$i)){
                $part1 += $i;
+            }
+            if(preg_match('/^(\d+)\1+$/', (string)$i)){
+               $part2 += $i;
             }
          }
       }        
       echo "part1: $part1\n";
+      echo "part2: $part2\n";
    }
 ?>
